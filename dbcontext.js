021961e3d54b24d9b3961bf10707118cab61207d
utkarshgupta178@gmail.com <ugupta@commvault.com>
2023-10-20 04:05:29 +0530

    Added mongo cluster to env

--- dbcontext.js
+++ dbcontext.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 // mongoose.connect('mongodb://localhost/yelpcamp',{useNewUrlParser : true,useUnifiedTopology : true});
 
-const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ej0msoj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 mongoose.connect(uri, {
   useNewUrlParser: true,
